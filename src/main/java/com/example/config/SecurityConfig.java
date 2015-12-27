b6b9a889359bbb57cd9cbfcf68b2642a55c1388a
zlabia <extradisk@yahoo.com>
2015-12-27 18:21:15 -0500

    Adding security and some thymeleaf templates

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.example.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     private static final String[] UNSECURED_RESOURCE_LIST = new String[] { "/resources/**",
@@ -11,6 +13,12 @@ public class SecurityConfig {
     private static final String[] UNAUTHORIZED_RESOURCE_LIST = new String[] { "/test.html", "/",
             "/unauthorized", "/error*", "/users*" };
 
-    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("user").roles("USER").and()
+                .withUser("admin").password("admin").roles("USER","ADMIN");
+    }
 	
 }
